735e511580d34ce7b768c04f625f70afc78d8609
Resh@308 <reshsingh1995@gmail.com>
2022-07-29 19:56:24 +0530

    Added:Ability to insert 30 between 56 and 70

--- src/LinkList/MainClass.java
+++ src/LinkList/MainClass.java
@@ -4,15 +4,16 @@ package LinkList;
 
 
 
+
 public class MainClass
 {
     public static void main(String[] args)
     {
 
         LinkedList list = new LinkedList();
-        list.push(56);
-        list.push(30);
-        list.push(70);
+        list.add(56);
+        list.add(70);
+        list.insertAfter(56,30);
         list.display();
     }
 }
